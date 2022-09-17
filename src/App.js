44aca83e544b2b8a19fcdfb6c14a142c432accf2
Mohamed <justmoe23@gmail.com>
2022-09-17 13:17:22 -0400

    integrate alan AI api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+const alanAPIKey =
+  "ad3f902c08ba1bf8144e10672821a09d2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanAPIKey,
+      onCommand: ({ command, body }) => {
+        if (command === 'newQuote') {
+          console.log("The quote is", JSON.parse(body));
+        }
+      },
+    });
+  }, []);
   return (
     <div>
       <h1> Nipsey's Quote of the Day</h1>
